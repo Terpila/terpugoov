42f152c15300034ad98db0e09dc5951ee5c7ccb2
terpila <terpugoov@bk.ru>
2021-05-08 01:08:22 +0600

    see please)

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int homo = 10;
+        System.out.println(homo);
+        String luv = ("Я люблю свою маму (Терпугова Анастасия Петровна, заместитель мэра города Омска) <3");
+        System.out.println(luv);
+        String lol = "ZibliClub)";
+        System.out.println(lol+luv);
+
+
+    }
+}
